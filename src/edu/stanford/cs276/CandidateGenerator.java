081b0aa50585ec5c07e6874a9a934795751f10f9
Ben Mittelberger <bmittelb@stanford.edu>
2016-04-24 15:05:22 -0700

    fixed some bugs and added transpose

--- src/edu/stanford/cs276/CandidateGenerator.java
+++ src/edu/stanford/cs276/CandidateGenerator.java
@@ -9,73 +9,81 @@ public class CandidateGenerator implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private static CandidateGenerator cg_;
 
-  // Don't use the constructor since this is a Singleton instance
-  private CandidateGenerator() {}
+	// Don't use the constructor since this is a Singleton instance
+	private CandidateGenerator() {
+	}
 
-  public static CandidateGenerator get() throws Exception {
-    if (cg_ == null) {
-      cg_ = new CandidateGenerator();
-    }
-    return cg_;
-  }
+	public static CandidateGenerator get() throws Exception {
+		if (cg_ == null) {
+			cg_ = new CandidateGenerator();
+		}
+		return cg_;
+	}
 
-  public static final Character[] alphabet = { 'a', 'b', 'c', 'd', 'e', 'f',
-      'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-      'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7',
-      '8', '9', ' ', ',' };
+	public static final Character[] alphabet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+			'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8',
+			'9', ' ', ',' };
 
-  // Generate all candidates for the target query
-  public static Set<String> getCandidates(String query) throws Exception {
-    Set<String> candidates = new HashSet<String>();
-    
-    String [] queryWords = query.trim().split(" ");
-    for (String word : queryWords) {
-      Set<String> wordCands = generateCandidateWords(word);
-    }
+	// Generate all candidates for the target query
+	public static Set<String> getCandidates(String query) throws Exception {
+		Set<String> candidates = new HashSet<String>();
 
-    return candidates;
-  }
+		String[] queryWords = query.trim().split(" ");
+		for (String word : queryWords) {
+			Set<String> wordCands = generateCandidateWords(word);
+		}
 
-  private static Set<String> generateCandidateWords(String word) {
-    Set<String> candidates = new HashSet<String>();
-    int wordLen = word.length();
-    System.out.println("Word: " + word);
-    
-    for (int i = 0 ; i < wordLen; i++) {
-    	String deleteWord = null;
-    	if (i != wordLen - 1) {
-    		deleteWord = word.substring(0,i) + word.substring(i + 1); //delete
-    	} else {
-    		deleteWord = word.substring(0,i);
-    	}
-        System.out.println("Delete[" + word.charAt(i) + "] : " + deleteWord);
-    	candidates.add(deleteWord);
-    }
-    
-    for (int i = 0; i < wordLen; i++) {
-      for (Character c : alphabet) {
-        String insertWord = "";
-        String replaceWord = "";
-        if (i == 0) {
-        	insertWord = c + word;
-        	replaceWord = c + word.substring(1);
-        } else if (i == wordLen -1 ){
-          insertWord = word + c;
-          replaceWord = word.substring(0,i) + c;
-        } else {
-          insertWord = word.substring(0,i) + c + word.substring(i); //insert
-          replaceWord = word.substring(0,i-1) + c + word.substring(i);
-        }
-        
-        System.out.println("Insert[" + c + "] : " + insertWord);
-        System.out.println("Replace[" + word.charAt(i) + "][" + c + "]: " + replaceWord);
+		return candidates;
+	}
 
-        candidates.add(insertWord);
-        candidates.add(replaceWord);
-      }
+	private static Set<String> generateCandidateWords(String word) {
+		Set<String> candidates = new HashSet<String>();
+		int wordLen = word.length();
+		System.out.println("Word: " + word);
 
-    }
-    return candidates;
-  }
+		for (int i = 0; i < wordLen; i++) {
+			String deleteWord = null;
+			if (i != wordLen - 1) {
+				deleteWord = word.substring(0, i) + word.substring(i + 1); // delete
+			} else {
+				deleteWord = word.substring(0, i);
+			}
+			System.out.println("Delete[" + word.charAt(i) + "] : " + deleteWord);
+			candidates.add(deleteWord);
+		}
+
+		for (int i = 0; i < wordLen; i++) {
+			for (Character c : alphabet) {
+				String insertWord = "";
+				String replaceWord = "";
+				if (i == 0) {
+					insertWord = c + word;
+					replaceWord = c + word.substring(1);
+				} else if (i == wordLen - 1) {
+					insertWord = word + c;
+					replaceWord = word.substring(0, i) + c;
+				} else {
+					insertWord = word.substring(0, i) + c + word.substring(i); // insert
+					replaceWord = word.substring(0, i - 1) + c + word.substring(i);
+				}
+
+				System.out.println("Insert[" + c + "] : " + insertWord);
+				System.out.println("Replace[" + word.charAt(i) + "][" + c + "]: " + replaceWord);
+
+				candidates.add(insertWord);
+				candidates.add(replaceWord);
+			}
+
+		}
+		for (int i = 0; i < wordLen - 1; i++) {
+			String transposeWord = word.substring(0, i) + word.charAt(i + 1) + word.charAt(i)
+					+ word.substring(i + 2);
+			candidates.add(transposeWord);
+			System.out.println("Transpose[" + word + "] --> " + transposeWord);
+
+		}
+
+		return candidates;
+	}
 
 }
--- src/edu/stanford/cs276/LanguageModel.java
+++ src/edu/stanford/cs276/LanguageModel.java
@@ -20,88 +20,94 @@ import edu.stanford.cs276.util.Pair;
  * (https://en.wikipedia.org/wiki/Singleton_pattern).
  */
 public class LanguageModel implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
-  private static LanguageModel lm_;
+	private static LanguageModel lm_;
 
-  Dictionary unigram = new Dictionary();
-  Dictionary bigram = new Dictionary();
+	Dictionary unigram = new Dictionary();
+	Dictionary bigram = new Dictionary();
 
-  /*
-   * Feel free to add more members here (e.g., a data structure that stores bigrams)
-   */
+	/*
+	 * Feel free to add more members here (e.g., a data structure that stores
+	 * bigrams)
+	 */
 
-  /**
-   * Constructor
-   * Do not call constructor directly from outside this class since this is a Singleton class
-   */
-  private LanguageModel(String corpusFilePath) throws Exception {
-    constructDictionaries(corpusFilePath);
-  }
+	/**
+	 * Constructor Do not call constructor directly from outside this class
+	 * since this is a Singleton class
+	 */
+	private LanguageModel(String corpusFilePath) throws Exception {
+		constructDictionaries(corpusFilePath);
+	}
 
-  /**
-   * This method computes language model parameters (i.e. counts of unigrams, bigrams etc)
-   */
-  public void constructDictionaries(String corpusFilePath) throws Exception {
+	/**
+	 * This method computes language model parameters (i.e. counts of unigrams,
+	 * bigrams etc)
+	 */
+	public void constructDictionaries(String corpusFilePath) throws Exception {
+		System.out.println("Constructing dictionaries...");
+		File dir = new File(corpusFilePath);
+		for (File file : dir.listFiles()) {
+			if (".".equals(file.getName()) || "..".equals(file.getName())) {
+				continue; // Ignore the self and parent aliases.
+			}
+			System.out.printf("Reading data file %s ...\n", file.getName());
+			BufferedReader input = new BufferedReader(new FileReader(file));
+			String line = null;
+			while ((line = input.readLine()) != null) {
+				String prevWord = "beginbegin";
+				String[] words = line.trim().split("\\s+");
+				for (String word : words) {
+					unigram.add(word);
+					String pair = word + "," + prevWord;
+					bigram.add(pair);
+					prevWord = word;
+				}
+			}
+			input.close();
+		}
+		System.out.println("Done.");
+	}
+	
+//	public boolean wordExists(String word) {
+//		return 
+//	}
 
-    System.out.println("Constructing dictionaries...");
-    File dir = new File(corpusFilePath);
-    for (File file : dir.listFiles()) {
-      if (".".equals(file.getName()) || "..".equals(file.getName())) {
-        continue; // Ignore the self and parent aliases.
-      }
-      System.out.printf("Reading data file %s ...\n", file.getName());
-      BufferedReader input = new BufferedReader(new FileReader(file));
-      String line = null;
-      while ((line = input.readLine()) != null) {
-        String prevWord = "beginbegin";
-        String[] words = line.trim().split("\\s+");
-        for (String word : words) {
-          unigram.add(word);
-	        String pair = word + "," + prevWord;
-	        bigram.add(pair);
-	        prevWord = word;
-        }
-      }
-      input.close();
-    }
-    System.out.println("Done.");
-  }
+	/**
+	 * Loads the language model object (and all associated data) from disk
+	 */
+	public static LanguageModel load() throws Exception {
+		try {
+			if (lm_ == null) {
+				FileInputStream fiA = new FileInputStream(Config.languageModelFile);
+				ObjectInputStream oisA = new ObjectInputStream(fiA);
+				lm_ = (LanguageModel) oisA.readObject();
+			}
+		} catch (Exception e) {
+			throw new Exception("Unable to load language model.  You may have not run build corrector");
+		}
+		return lm_;
+	}
 
-  /**
-   * Loads the language model object (and all associated data) from disk
-   */
-  public static LanguageModel load() throws Exception {
-    try {
-      if (lm_ == null) {
-        FileInputStream fiA = new FileInputStream(Config.languageModelFile);
-        ObjectInputStream oisA = new ObjectInputStream(fiA);
-        lm_ = (LanguageModel) oisA.readObject();
-      }
-    } catch (Exception e) {
-      throw new Exception("Unable to load language model.  You may have not run build corrector");
-    }
-    return lm_;
-  }
+	/**
+	 * Saves the object (and all associated data) to disk
+	 */
+	public void save() throws Exception {
+		FileOutputStream saveFile = new FileOutputStream(Config.languageModelFile);
+		ObjectOutputStream save = new ObjectOutputStream(saveFile);
+		save.writeObject(this);
+		save.close();
+	}
 
-  /**
-   * Saves the object (and all associated data) to disk
-   */
-  public void save() throws Exception {
-    FileOutputStream saveFile = new FileOutputStream(Config.languageModelFile);
-    ObjectOutputStream save = new ObjectOutputStream(saveFile);
-    save.writeObject(this);
-    save.close();
-  }
-
-  /**
-   * Creates a new LanguageModel object from a corpus. This method should be used to create a
-   * new object rather than calling the constructor directly from outside this class
-   */
-  public static LanguageModel create(String corpusFilePath) throws Exception {
-    if (lm_ == null) {
-      lm_ = new LanguageModel(corpusFilePath);
-    }
-    return lm_;
-  }
+	/**
+	 * Creates a new LanguageModel object from a corpus. This method should be
+	 * used to create a new object rather than calling the constructor directly
+	 * from outside this class
+	 */
+	public static LanguageModel create(String corpusFilePath) throws Exception {
+		if (lm_ == null) {
+			lm_ = new LanguageModel(corpusFilePath);
+		}
+		return lm_;
+	}
 }
