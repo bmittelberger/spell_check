4becf54cc6e1aa3e2b629afdebde2896e94ea67d
Ben Mittelberger <bmittelb@stanford.edu>
2016-04-24 14:37:06 -0700

    first real commit

---
+++ src/edu/stanford/cs276/CandidateGenerator.java
@@ -0,0 +1,80 @@
+package edu.stanford.cs276;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+public class CandidateGenerator implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private static CandidateGenerator cg_;
+
+  // Don't use the constructor since this is a Singleton instance
+  private CandidateGenerator() {}
+
+  public static CandidateGenerator get() throws Exception {
+    if (cg_ == null) {
+      cg_ = new CandidateGenerator();
+    }
+    return cg_;
+  }
+
+  public static final Character[] alphabet = { 'a', 'b', 'c', 'd', 'e', 'f',
+      'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+      'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7',
+      '8', '9', ' ', ',' };
+
+  // Generate all candidates for the target query
+  public static Set<String> getCandidates(String query) throws Exception {
+    Set<String> candidates = new HashSet<String>();
+    
+    String [] queryWords = query.trim().split(" ");
+    for (String word : queryWords) {
+      Set<String> wordCands = generateCandidateWords(word);
+    }
+
+    return candidates;
+  }
+
+  private static Set<String> generateCandidateWords(String word) {
+    Set<String> candidates = new HashSet<String>();
+    int wordLen = word.length();
+    System.out.println("Word: " + word);
+    
+    for (int i = 0 ; i < wordLen; i++) {
+    	String deleteWord = null;
+    	if (i != 0) {
+    		deleteWord = word.substring(0,i-1) + word.substring(i); //delete
+    	} else {
+    		deleteWord = word.substring(1);
+    	}
+        System.out.println("Delete[" + word.charAt(i) + "] : " + deleteWord);
+    	candidates.add(deleteWord);
+    }
+    
+    for (int i = 0; i < wordLen; i++) {
+      for (Character c : alphabet) {
+        String insertWord = "";
+        String replaceWord = "";
+        if (i == 0) {
+
+        } else if (i == wordLen -1 ){
+          insertWord = word + c;
+          replaceWord = word.substring(0,i-1) + c + word.substring(i);
+        } else {
+          insertWord = word.substring(0,i) + c + word.substring(i); //insert
+          replaceWord = word.substring(0,i-1) + c + word.substring(i);
+        }
+        
+        System.out.println("Insert[" + c + "] : " + insertWord);
+        System.out.println("Replace[" + word.charAt(i) + "][" + c + "]: " + replaceWord);
+
+        candidates.add(insertWord);
+        candidates.add(replaceWord);
+      }
+
+    }
+    return candidates;
+  }
+
+}
