bbb7050d9165a690312c800c6f1bdf7316c5700e
Matthew Lamm <matthew.r.lamm@gmail.com>
2016-04-28 14:39:12 -0700

    updated uniform cost model

--- src/edu/stanford/cs276/UniformCostModel.java
+++ src/edu/stanford/cs276/UniformCostModel.java
@@ -15,36 +15,46 @@ public class UniformCostModel implements EditCostModel {
 	private static double mu = 1.0; // the dampening factor on language model p(Q)
 	private static double lambda = .5; // the interpolation parameter for language model smoothing
   @Override
-  public double editProbability(ArrayList<String> original, PossibleQuery Q, int distance ) throws Exception {
+  public double editProbability(ArrayList<String> original , PossibleQuery Q, int distance ) throws Exception {
 	 
 	  double p_r_q = 1; 
 	  double p_q = 1;
 	  
-	
+
 	  LanguageModel lm = LanguageModel.load();
 
 	  // compute P( R | Q ) 
 	  int count_mistakes = 0;
-	  for ( int i = 0 ; i < original.size() && i < Q.getQuery().size(); i ++ ){
-		  if ( ! original.get( i ).equals( Q.getQuery().get( i ).getFirst() ) ) {
-			  count_mistakes += 1;
+	  ArrayList<Pair<String, String> > word_pairs = Q.getQuery();
+	  ArrayList<String> candidate_words = new ArrayList<String>();
+	  
+	  for ( Pair<String,String> pair : word_pairs ){
+		  String[] words = pair.getFirst().split( " " );
+		  String edit = pair.getSecond() ;
+		  if ( ! edit.equals( "none" ) ) {
+			  count_mistakes++ ;
+		  }
+		  for ( int i = 0 ; i < words.length ; i ++ ){
+			  candidate_words.add( words[i ] ); 
 		  }
 	  }
+	  
 	  if (count_mistakes > 0) {
 		  p_r_q = Math.pow( editCost , (double) count_mistakes ); 
 	  }
 	  else{ 
 		  p_r_q = identityProbability; 
 	  }
-	  
-	  for ( int i = 0 ; i < Q.getQuery().size(); i ++  ){
+	 
+	  for ( int i = 0 ; i < candidate_words.size(); i ++  ){
+		  //String[] words = word_pairs.get(i).getFirst().split( " " ) ;
 		  if (i == 0) {
-			  double prob = (double)lm.unigram.count( Q.getQuery().get( i ).getFirst() ) / (double)lm.unigram.getTermCount() ; 
+			  double prob = (double) lm.unigram.count( candidate_words.get( i ) ) / (double)lm.unigram.getTermCount() ; 
 			  p_q *= prob ;
 		  }
 		  else {
-			  double unigram_prob = (double)lm.unigram.count( Q.getQuery().get( i ).getFirst() ) / (double)lm.unigram.getTermCount();
-			  double bigram_prob = (double)lm.bigram.count( Q.getQuery().get( i - 1 ).getFirst() + "," + (Q.getQuery().get( i ).getFirst() )) / (double)lm.unigram.count( Q.getQuery().get(i - 1 ).getFirst());
+			  double unigram_prob = (double)lm.unigram.count( candidate_words.get( i ) ) / (double)lm.unigram.getTermCount();
+			  double bigram_prob = (double)lm.bigram.count( candidate_words.get( i - 1 ) + "," + candidate_words.get( i ) ) / (double)lm.unigram.count( candidate_words.get( i - 1 ) );
 			  double interp = lambda*unigram_prob + ( 1.0 - lambda)*bigram_prob;
 			  p_q *= interp;
 		  }
